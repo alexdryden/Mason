3de96a99bd94904e428ff287d75c2adcae36a7e9
Alex Dryden <adryden3@illinois.edu>
2020-11-24 21:23:50 -0500

    Change match to exhibts depth -1
    When the user inputs 0 as the depth, then the form field gets
    parsed as empty or false. So, we will have to communicate the
    depth as 1 instead of 0

--- src/Site/BlockLayout/ListOfExhibits.php
+++ src/Site/BlockLayout/ListOfExhibits.php
@@ -130,7 +130,7 @@ class ListOfExhibits extends AbstractBlockLayout
 
         //filter array for values that match given depth
         foreach ($indents as $page_id => $depth):
-            if ($depth == $exhibits_depth){
+            if ($depth == $exhibits_depth-1){
                 $exhibits[$page_id] = $this->getPreview($page_id, $default_img,'large', $view);
             }
         endforeach;
